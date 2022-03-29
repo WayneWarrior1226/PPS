58277e53cae95321103dc8a754189da0377e0c10
Abbey Noyes <abbeynoyes@gmail.com>
2022-03-29 14:22:47 -0400

    Update
    I am still trying to get the new user input to insert into the sql workbench database

--- src/main/java/UserDAO.java
+++ src/main/java/UserDAO.java
@@ -1,4 +1,56 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.io.IOException;
 
-public class UserDAO {
+public class UserDAO{
+	
+	private String jdbcURL = "jdbc:mysql://localhost:3306/pps";
+	private String jdbcUsername = "root";
+	private String jdbcPassword = "pass1234";
+	private PreparedStatement preparedStatement = null;
+	
+	public static final String INSERT_USERS_SQL =  "INSERT INTO users" + "(email, password, fname,lname,birthday,streetnum,street,city,state,zip, dollaramount,ppsamount) VALUES"
+				+ "(?, ?,?,?, ?,?,?,?,?,?,?,?);";
+		
+	//GET CONNECTION
+	protected Connection getConnection() {
+		Connection connection = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return connection;
+	}
+	
+	//Add new user
+	public void insertUser(Users users) throws SQLException {
+		try(Connection connection = getConnection();
+				PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL)){
+			preparedStatement.setString(1, users.getEmail());
+			preparedStatement.setString(2, users.getPassword());
+			preparedStatement.setString(3, users.getFname());
+			preparedStatement.setString(4, users.getLname());
+			preparedStatement.setString(5, users.getBirthday());
+			preparedStatement.setString(6, users.getStreetnum());
+			preparedStatement.setString(7, users.getStreet());
+			preparedStatement.setString(8, users.getCity());
+			preparedStatement.setString(9, users.getState());
+			preparedStatement.setString(10, users.getZip());
+			preparedStatement.setString(11, users.getDollaramount());
+			preparedStatement.setString(12, users.getPpsamount());
+			preparedStatement.executeUpdate();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	}  
 
-}
+
+
+
--- src/main/java/Users.java
+++ src/main/java/Users.java
@@ -7,127 +7,116 @@
  */
 
 public class Users{
-	protected String userid;
+	protected String email;
 	protected String password;
 	protected String fname;
 	protected String lname;
-	protected int birthday;
-	protected int streetnum;
+	protected String birthday;
+	protected String streetnum;
 	protected String street;
 	protected String city;
 	protected String state;
-	protected int zip;
-
-
-public Users() {
-}
-
-public Users(String userid) {
-	this.userid = userid;
-}
-public Users(String userid, String password, String fname, String lname, int birthday, 
-		int streetnum, String street, String city, String state,int zip) {
-	this(password,fname,lname,birthday,streetnum,street,city,state,zip);
-	this.userid = userid;
-}
-
-public Users(String password, String fname, String lname, int birthday, 
-		int streetnum, String street, String city, String state,int zip) {
-	this.password = password;
-	this.fname=fname;
-	this.lname=lname;
-	this.birthday = birthday;
-	this.streetnum = streetnum;
-	this.street = street;
-	this.city=city;
-	this.state = state;
-	this.zip=zip;
-}
-/*get user id
- *
-*/
-public String getId() {
-	return userid;
-}
-
-public void setUserId(String userid) {
-	this.userid=userid;
-}
-
-public String getPassword(){
-	return password;
-}
-
-public void setPassword(String password) {
-	this.password = password;
-}
-
-public String getfname() {
-	return fname;
-}
-
-public void setfname(String fname) {
-	this.fname=fname;
-}
-public String getLname() {
-	return lname;
-}
-public void setLname(String lname) {
-	this.lname=lname;
-}
-
-public int getBirthday() {
-	return birthday;
-}
-
-public void setBirthday(int birthday) {
+	protected String zip;
+	protected String dollaramount;
 	
-	this.birthday = birthday;
-}
 
-public int getStreetnum() {
-	return streetnum;
-}
-
-public void setStreetnum(int streetnum) {
-	this.streetnum = streetnum;
-}
-
-public String getStreet() {
-	return street;
-}
-
-public void setStreet(String street) {
-	this.street = street;
-}
-
-public String getCity() {
-	return city;
-}
-
-public void setCity(String city) {
-	this.city=city;
-}
-
-public String getState() {
-	return state;
-}
-
-public void setState(String state) {
-	this.state = state;
-}
-
-public int getZip() {
-	return zip;
 	
-}
-
-public void setZip(int zip) {
-	this.zip = zip;
 	
-}
-}
+	
+	public Users(String password, String fname, String lname, String birthday, String streetnum, String street, String city,
+			String state, String zip, String dollaramount, String ppsamount) {
+		super();
+		this.password = password;
+		this.fname = fname;
+		this.lname = lname;
+		this.birthday = birthday;
+		this.streetnum = streetnum;
+		this.street = street;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.dollaramount = dollaramount;
+		this.ppsamount = ppsamount;
+	}
 
+
+
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	public String getStreetnum() {
+		return streetnum;
+	}
+	public void setStreetnum(String streetnum) {
+		this.streetnum = streetnum;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getZip() {
+		return zip;
+	}
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+	public String getDollaramount() {
+		return dollaramount;
+	}
+	public void setDollaramount(String dollaramount) {
+		this.dollaramount = dollaramount;
+	}
+	public String getPpsamount() {
+		return ppsamount;
+	}
+	public void setPpsamount(String ppsamount) {
+		this.ppsamount = ppsamount;
+	}
+	protected String ppsamount;
+
+
+}
 
 
 
